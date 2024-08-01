77179c8d7bab65432ba2823dda8143fcdd27dbe4
daikj <tyutyunniksolomia@gmail.com>
2024-08-01 18:47:39 +0300

    create base.html, index.html

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from todoapp.models import Task, Tag
+
+
+def index(request):
+    """View function for the home page of the site."""
+    tasks = Task.objects.all()
+    num_tasks = Task.objects.count()
+    num_tags = Tag.objects.count()
+
+    context = {
+        "tasks": tasks,
+        "num_tasks": num_tasks,
+        "num_tags": num_tags,
+    }
+    return render(request, "todoapp/index.html", context=context)
